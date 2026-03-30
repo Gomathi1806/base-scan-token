29e3a0ea01ecd91f41d2dece95c0e1fab0e60cbb
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2026-03-30 14:14:00 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async redirects() {
@@ -8,12 +7,10 @@ const nextConfig: NextConfig = {
         source: "/.well-known/farcaster.json",
         destination:
           "https://api.farcaster.xyz/miniapps/hosted-manifest/019d3e80-f692-59f3-67f1-5b1d4af4006f",
-        permanent: false, // 307 temporary redirect
+        permanent: false,
       },
     ];
   },
 };
 
-export default nextConfig;
-const nextConfig: NextConfig = {};
 export default nextConfig;
